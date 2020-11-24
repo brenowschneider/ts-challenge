bf6fcc159422cdfbd1c5b9ce41b8277255a7d12a
Breno Wruck Schneider <brenowschneider@gmail.com>
2020-11-24 11:42:13 +0100

    adds unit test for product comparison service

--- src/services/productComparisonService.ts
+++ src/services/productComparisonService.ts
@@ -1,21 +1,35 @@
-import { ComparableProduct } from "./interfaces/comparable-product";
-import { Product } from "./interfaces/product";
-import { BasicElectricityTariff } from "./products/basic-electricity-tariff";
-import { PackagedTariff } from "./products/packaged-tariff";
+import { ComparableProduct } from './interfaces/comparable-product';
+import { Product } from './interfaces/product';
+import { BasicElectricityTariff } from './products/basic-electricity-tariff';
+import { PackagedTariff } from './products/packaged-tariff';
 
 const products: Array<ComparableProduct> = [
-    new BasicElectricityTariff(),
-    new PackagedTariff()
-]
+  new BasicElectricityTariff(),
+  new PackagedTariff(),
+];
 
-export const compareProductConsumePerYear = (consumeInKWh: number): Array<Product> => {
-    const mappedProducts = products.map<Product>(product => {
-      return {
-        name: product.name,
-        annualCostsInEuros: product.calculateAnnualCostsInEuro(consumeInKWh)
-      }
-    })
+const sortProductsByCostsAscending = (products: Array<Product>) => {
+  return products.sort(
+    (product1, product2) =>
+      product1.annualCostsInEuros - product2.annualCostsInEuros
+  );
+};
 
-    return mappedProducts.sort((product1, product2) => product1.annualCostsInEuros - product2.annualCostsInEuros)
+export const compareProductConsumePerYear = (
+  consumeInKWh: number
+): Array<Product> => {
+  if (consumeInKWh <= 0) {
+    throw new Error(
+      'Please pass a valid consume (e.g. 1000, 2000) for comparing the product costs'
+    );
+  }
 
-}
+  const mappedProducts = products.map<Product>((product) => {
+    return {
+      name: product.name,
+      annualCostsInEuros: product.calculateAnnualCostsInEuro(consumeInKWh),
+    };
+  });
+
+  return sortProductsByCostsAscending(mappedProducts);
+};
---
+++ src/tests/services/productComparisonService.spec.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import 'mocha';
+import { compareProductConsumePerYear } from '../../services/productComparisonService';
+
+describe('First test', () => {
+  it('should return "Basic Electricity Tariff" as first element when consume is 2000', () => {
+    const result = compareProductConsumePerYear(2000);
+    expect(result).to.eql([
+      { name: 'Basic Electricity Tariff', annualCostsInEuros: 500 },
+      { name: 'Packaged Tariff', annualCostsInEuros: 800 },
+    ]);
+  });
+
+  it('should return "Packaged Tariff" as first element when consume is 4000', () => {
+    const result = compareProductConsumePerYear(4000);
+    expect(result).to.eql([
+      { name: 'Packaged Tariff', annualCostsInEuros: 800 },
+      { name: 'Basic Electricity Tariff', annualCostsInEuros: 940 },
+    ]);
+  });
+
+  it('should return "Packaged Tariff" as first element when consume is 4000', () => {
+    const result = compareProductConsumePerYear(6000);
+    expect(result).to.eql([
+      { name: 'Basic Electricity Tariff', annualCostsInEuros: 1380 },
+      { name: 'Packaged Tariff', annualCostsInEuros: 1400 },
+    ]);
+  });
+});
